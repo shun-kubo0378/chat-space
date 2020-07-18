12386ab77a9267e0c12a706a1e247dfe203c61d7
shun-kubo8730 <bbpks080730@gmail.com>
2020-07-18 23:05:16 +0900

    Asynchronous message sending

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,69 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html = 
+      `<div class="message__box">
+          <div class="message__box__info">
+            <div class="message__box__info__user-name">
+              ${message.user_name}
+            </div>
+            <div class="message__box__info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__box__content">
+            <p class="message__box__content-message">
+              ${message.content}
+            </p>
+            <img class="message__box__content-image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="message__box">
+        <div class="message__box__info">
+          <div class="message__box__info__user-name">
+            ${message.user_name}
+          </div>
+          <div class="message__box__info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message__box__content">
+          <p class="message__box__content-message">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+    
+  $('.new_message').on('submit', function(e){
+    e.preventDefault()
+    let formData = new FormData(this)
+    let url = $(this).attr('action')
+    
+    $.ajax ({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.message').append(html);    
+      $('.message').animate({ scrollTop: $('.message')[0].scrollHeight});  
+      $('form')[0].reset();
+      $('.submit-btn').removeAttr('disabled')
+    })
+
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+  });
+});
